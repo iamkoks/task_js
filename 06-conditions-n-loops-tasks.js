d824fba365a7bf4d1ffb4261c2c8908c846e7999
iamkoks <iamkoks@users.noreply.github.com>
2020-07-15 15:39:53 +0300

    getFizzBuzz

--- 06-conditions-n-loops-tasks.js
+++ 06-conditions-n-loops-tasks.js
@@ -29,8 +29,22 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(num) {
+const getFizzBuzz = (num) => {
+    if(num % 15 == 0) {
+        return 'FizzBuzz'
+    }
+    else if(num % 5 == 0) {
+        return 'Buzz'
+    }
+    else if(num % 3 == 0) {
+        return 'Fizz'
+    }
+    else {
+        return num
+    }
 }
+
+console.log(getFizzBuzz(20))
 
 
 /**
